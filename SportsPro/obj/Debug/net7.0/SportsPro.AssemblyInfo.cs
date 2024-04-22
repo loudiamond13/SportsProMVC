579e6c7bdc9c5654d8fe07cc277c7062b9f5a54c
Lou Diamond Loyloy <loudiamond_loyloy@insideranken.org>
2024-04-22 08:45:55 -0500

    Initial

--- SportsPro/obj/Debug/net7.0/SportsPro.AssemblyInfo.cs
+++ SportsPro/obj/Debug/net7.0/SportsPro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportsPro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7063bf5ef7948766671daa1a02cca7e2f520a7c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportsPro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportsPro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
